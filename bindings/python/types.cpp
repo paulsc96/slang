211209142fb226f63682334ad85962fbec26939a
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2022-11-04 13:14:17 +0100

    bindings/python: Add new TypePrinter options

--- bindings/python/types.cpp
+++ bindings/python/types.cpp
@@ -119,6 +119,8 @@ void registerTypes(py::module_& m) {
         .def_readwrite("elideScopeNames", &TypePrintingOptions::elideScopeNames)
         .def_readwrite("printAKA", &TypePrintingOptions::printAKA)
         .def_readwrite("anonymousTypeStyle", &TypePrintingOptions::anonymousTypeStyle);
+        .def_readwrite("skipScopedTypeNames", &TypePrintingOptions::anonymousTypeStyle);
+        .def_readwrite("fullEnumType", &TypePrintingOptions::anonymousTypeStyle);
 
     py::enum_<TypePrintingOptions::AnonymousTypeStyle>(typePrintingOptions, "AnonymousTypeStyle")
         .value("SystemName", TypePrintingOptions::SystemName)
